510a78692b38370ee1ec8664649adee8dc3e135f
AJM <andrewmora2860@gmail.com>
2018-09-20 12:15:57 -0400

    first

---
+++ exercise_1.1.py
@@ -0,0 +1,5 @@
+# Andrew Mora
+# hello_world.py
+
+#this line prints out hello world
+print ("hello world") \
---
+++ hw1.py
@@ -0,0 +1,109 @@
+ # Name: Andrew Mora
+# Section:
+# Date: 9/19/2018
+# hw1.py
+import math
+##### Template for Homework 1, exercises 1.2-1.5 ######
+
+print( "********** Exercise 1.2 **********")
+print("    |     |")
+print("--------------")
+print("    |     |")
+print("--------------")
+print("    |     |")
+
+
+print ("********** Exercise 1.3 **********")
+
+a="    |     |"
+b="--------------"
+
+print (a)
+print (b)
+print (a)
+print (b)
+print (a)
+print("Not implemented") # Delete this line when you write your code!
+
+
+print("********** Exercise 1.4 **********")
+
+print("********* Part I *************")
+
+#1
+print("part 1")
+a=5/2
+b=5/2.0
+c=5.0/2
+print(a)
+print(b)
+print(c)
+
+print("part 2")
+#2
+a=7*(1/2)
+b=7*(1/2.0)
+print(a)
+print(b)
+
+
+print("part 3")
+#3
+a=5**2
+b=5.0**2
+c=5**2.0
+print(a)
+print(b)
+print(c)
+
+print("part 4")
+#4
+a=1/3.0
+print(a)
+
+print("********* Part II *************")
+
+#1
+a=(3*5)/(2+3)
+print("#1")
+print(a)
+
+#2
+b= math.sqrt(7+9)*2
+print("#2")
+print(b)
+
+#3
+a=(4-7)**3
+print("#3")
+print(a)
+
+#4
+a=((-19+100)**(1./4))
+print("#4")
+print(a)
+
+#5
+a=(6%4)
+print("5")
+print(a)
+
+
+print("********* Part III *************")
+
+print(5+1*10)
+print((5+1)*10)
+
+
+
+print("********** Exercise 1.5 **********")
+
+print("Not implemented") # Delete this line when you write your code!
+
+firstname=input("first name")
+lastname=input("last name")
+month=input("month")
+day=input("day")
+year=input("year")
+
+print(firstname, lastname, month, day, year)
