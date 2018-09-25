1a8127b6c0a510cf01861d1faf9f03d9b8ac4e57
AJM <andrewmora2860@gmail.com>
2018-09-25 13:22:30 -0400

    Finished exercise 1.8 #2

--- loops.py
+++ loops.py
@@ -1,15 +1,35 @@
 
 
-for n in [1.0, 2.0, 3.0, 4, 5, 6, 7, 8, 9, 10]:
+for n in [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]:
 
        print(1/n)
 
-print(type(n))
-float(n)
-print(type(n))
-
-n = input("enter number for countdown\n")
-while n < 0:
-    n = input("enter number for countdown\n")
-    if n > 0:
+# found this try and catch code which is nice to rude out bad inputs.
+while True:
+    try:
+        # Note: Python 2.x users should use raw_input, the equivalent of 3.x's input
+        n = int(input("enter number for countdown\n"))
+    except ValueError:
+        print("Sorry, I didn't understand that.")
+        #better try again... Return to the start of the loop
+        continue
+    else:
+        #age was successfully parsed!
+        #we're ready to exit the loop.
         break
+
+
+b = int(n)
+
+while b <= 0:
+    n = input("enter a positive number for countdown\n")
+    b = int(n)
+    if b >= 0:
+        break
+while b > 0:
+    b -= 1
+    if b == 0:
+        print(b)
+        break
+    print(b)
+
