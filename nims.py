92b0e22a7a952e9a5a95db2d726348f36d48cb7a
AJM <andrewmora2860@gmail.com>
2018-10-03 07:33:48 -0400

    Started to add all the conditions for winning and how to track it all. I need to move all of the code into the function.

--- nims.py
+++ nims.py
@@ -9,8 +9,12 @@ def play_nims(pile, max_stones):
     @param max_stones: the maximum number of stones you can take on one turn
     '''
 
+
+
     ## Basic structure of program (feel free to alter as you please):
+#this flag is used to keep track of correct responses
 flag = False
+
 
 while flag != True:
 
@@ -20,18 +24,41 @@ while flag != True:
  except ValueError:
    print("That's not a number")
 
-flag = False
+flag = False #this ensures that the loop to ask player one for how many stones is entered.
 
-while flag != True:
-
- try:
-  max_stones = int(input("How many stones do you want to pull 1-5?"))
-
-  if max_stones == 1 or 2 or 3 or 4 or 5:
+print("the amount of rocks remaining is", pile)
+while pile > 0: #loop that will run until the pile of rocks goes to zero
+  flag = False
+ while flag != True:
+#player ones turn starts
+  try:
+   max_stones = int(input("How many stones do you want to pull Player one 1-5?"))
+   pile = pile - max_stones
+   print("the amount of rocks left is", pile)
+   winner = "Player one"
+   if pile <=0:
+       print(winner)
+   if max_stones == 1 or 2 or 3 or 4 or 5:
     flag = True
    else:
-      print("That is not a number between 1-5")
- except ValueError:
+    print("That is not a number between 1-5")
+  except ValueError:
+   print("That's not a number")
+
+#player two turn starts
+ print("the amount of rocks remaining is", pile)
+ flag = False
+ while flag != True:
+
+  try:
+   max_stones = int(input("How many stones do you want to pull Player two 1-5?"))
+   pile = pile - max_stones
+   print("the amount of rocks left is", pile)
+   if max_stones == 1 or 2 or 3 or 4 or 5:
+    flag = True
+   else:
+    print("That is not a number between 1-5")
+  except ValueError:
    print("That's not a number")
 
 
