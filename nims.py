4f5fba4ad5d4532f647baff1ad9b6c86be5e390a
AJM <andrewmora2860@gmail.com>
2018-10-10 12:37:16 -0400

    Nims is complete. Dirty but functional.

--- nims.py
+++ nims.py
@@ -17,26 +17,27 @@ def stones(flag, player, pile):
   global pile1
   while flag != True: #players turn starts
    try:
-    print(str(pile1) + "inside of stones")
     if pile1 > 0: # this is in case there is no pile left when the choice gets to player one
      max_stones = int(input("How many stones do you want to pull" + player + "1-5"))
      if max_stones <= 0:
          max_stones = 0
          print(" stones entered was 0 or a negative")
-     if max_stones == 5 or 4 or 3 or 2 or 1:
-         flag = True
-         pile1 = pile1 - max_stones
-         if pile1 <= 0:
+     if max_stones > 0:
+         if max_stones < 6:
+          flag = True
+          pile1 = pile1 - max_stones
+          if pile1 <= 0:
              print("There are no rocks left")
              pile1 = 0
              return
-         print(str(pile1) + "leaving stones")
-         return
+          return
+         else:
+             print("That is not a number between 1-5")
      else:
-         print("That is not a number between 1-5")
+      print("That is not a number between 1-5")
     else:
-      print("There are", pile1, "rocks left.")
-    print(max_stones)
+     print("There are", pile1, "rocks left.")
+     return
    except ValueError:
      print("That's not a number")
   print(str(pile1) + "in loop")
@@ -44,24 +45,28 @@ def stones(flag, player, pile):
 def stones_game():
   global pile1
   global player
-  try:
-    pile = int(input("How large of a pile of stones do you want?"))
-    if pile > 1:
-     pile1 = pile
-
-  except ValueError:
+  flag = False
+  while flag == False:
+   try:
+     while flag == False:
+      pile = int(input("How large of a pile of stones do you want?"))
+      if pile > 1:
+       pile1 = pile
+       flag = True
+      else:
+         print("That is not a positive number.")
+   except ValueError:
         print("That's not a number")
 
-  flag = False  # this flag is used to keep track of correct responses
-  print(str(pile) + "begining of stones_game")
+
   while pile1 > 0: #loop that will run until the pile of rocks goes to zero
    flag = False
    player = "player one"
-   print(str(pile1) + " before stones player one")
+   print(str(player) + " The pile is now " + str(pile1))
    stones(flag, player, pile1)
-   print(str(pile1) + " pile1 value")
+
    player = "player two"
-   print(str(pile1) + " before player two")
+   print(str(player) +" The pile is now " + str(pile1) )
    stones(flag, player, pile1)
   return pile1
 
