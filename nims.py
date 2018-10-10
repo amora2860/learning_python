7d0bd6f15997fef5552d47f271c6643f6fb88e11
AJM <andrewmora2860@gmail.com>
2018-10-10 07:38:23 -0400

    nims is working now the onl issue is a negaive value is passed and an error should come up.

--- nims.py
+++ nims.py
@@ -2,75 +2,69 @@
 # Section:
 # nims.py
 
-
-
+# this function it ment to ask the player how many stones do they want in the pile
 def ask_pile():
-  global pile
+  global pile1
   try:
      pile = int(input("How large of a pile of stones do you want?"))
+     pile1 = pile
      if pile < 1:
-      return pile
+      return pile, pile1
   except ValueError:
      print("That's not a number")
 
 def stones(flag, player, pile):
-  global max_stones
+  global pile1
   while flag != True: #players turn starts
    try:
-    if pile > 0: # this is in case there is no pile left when the choice gets to player one
+    print(str(pile1) + "inside of stones")
+    if pile1 > 0: # this is in case there is no pile left when the choice gets to player one
      max_stones = int(input("How many stones do you want to pull" + player + "1-5"))
-     pile = pile - max_stones
-     print(pile)
-     if pile <= 0:
-      print("There are no rocks left")
+     if max_stones <= 0:
+         max_stones = 0
+         print(" stones entered was 0 or a negative")
+     if max_stones == 5 or 4 or 3 or 2 or 1:
+         flag = True
+         pile1 = pile1 - max_stones
+         if pile1 <= 0:
+             print("There are no rocks left")
+             pile1 = 0
+             return
+         print(str(pile1) + "leaving stones")
+         return
      else:
-      print("There are", pile, "rocks left.")
-    if max_stones == 1 or 2 or 3 or 4 or 5:
-     flag = True
-     return
+         print("That is not a number between 1-5")
     else:
-      print("That is not a number between 1-5")
+      print("There are", pile1, "rocks left.")
+    print(max_stones)
    except ValueError:
      print("That's not a number")
-  print(str(pile) + "in loop")
-  return pile, max_stones
+  print(str(pile1) + "in loop")
 
 def stones_game():
-  global pile
+  global pile1
+  global player
+  try:
+    pile = int(input("How large of a pile of stones do you want?"))
+    if pile > 1:
+     pile1 = pile
+
+  except ValueError:
+        print("That's not a number")
+
   flag = False  # this flag is used to keep track of correct responses
-  print(pile)
-  while pile > 0: #loop that will run until the pile of rocks goes to zero
+  print(str(pile) + "begining of stones_game")
+  while pile1 > 0: #loop that will run until the pile of rocks goes to zero
    flag = False
    player = "player one"
-   stones(flag, player, pile)
+   print(str(pile1) + " before stones player one")
+   stones(flag, player, pile1)
+   print(str(pile1) + " pile1 value")
    player = "player two"
-   print(pile)
-   stones(flag, player, pile)
-  return
+   print(str(pile1) + " before player two")
+   stones(flag, player, pile1)
+  return pile1
 
-def stones(flag, player, pile):
-  global max_stones
-  while flag != True: #players turn starts
-   try:
-    if pile > 0: # this is in case there is no pile left when the choice gets to player one
-     max_stones = int(input("How many stones do you want to pull" + player + "1-5"))
-     pile = pile - max_stones
-     print(pile)
-     if pile <= 0:
-      print("There are no rocks left")
-     else:
-      print("There are", pile, "rocks left.")
-    if max_stones == 1 or 2 or 3 or 4 or 5:
-     flag = True
-     return
-    else:
-      print("That is not a number between 1-5")
-   except ValueError:
-     print("That's not a number")
-  print(str(pile) + "in loop")
-  return pile, max_stones
-
-
-ask_pile()
+global player
 stones_game()
-print("Game over the winner is ")
+print("Game over the winner is " + player )
